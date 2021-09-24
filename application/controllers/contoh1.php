227e5709ff21a37d1bd2e11a4531c9e2e79374e4
latihan.git <zafardarmansyah30@gmail.com>
2021-09-24 21:48:44 +0700

    commit pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1";
+        echo "Nama saya Zafar Darmansyah
+            Saya tinggal di daerah Pondok kelapa
+            olah raga yang saya sukai adalah
+            yang menyenangkan";
+            
+    }
+}
---
+++ application/controllers/latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        //$this->load->view('view-latihan1);
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('model_latihan1');
+        //$hasil = $this->model_latihan1->jumlah($n1, $n2);
+        //echo "hasil penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil;
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil']  = $this->model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan', $data);
+    }
+}
